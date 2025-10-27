969ef8b2337b6ba6f061b8b79f1c96af45548c73
naamamaatra <07nagarajhegde@gmail.com>
2025-10-27 12:39:20 +0530

    Update email configurations in config.py

--- spamguard/config.py
+++ spamguard/config.py
@@ -1,11 +1,12 @@
 SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
 
 # Source Gmail ID (where all mails are received)
-SOURCE_EMAIL = "cartnow.1@gmail.com"
+SOURCE_EMAIL = "sourcegmailid@gmail.com"
 
 # Department Gmail IDs (for routing)
 DEPARTMENT_EMAILS = {
-    "support": "suprt.cartnow@gmail.com",
-    "sales": "sales.cartnow1@gmail.com",
-    "tech": "tech.cartnow1@gmail.com"
+    "support": "suprt@gmail.com",
+    "sales": "sales@gmail.com",
+    "tech": "tech@gmail.com"
 }
+
